6c471b50a30d4f8b2f85a5a1f0ed219db7004232
Allen Young <vegaswarrior888@gmail.com>
2023-08-09 15:52:25 -0700

    First Comment

---
+++ app/page.js
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+      <main>
+        <div> 
+        <h1 className='text-center text-blue-800 text-6xl my-44' >Warrior Web Designs</h1>
+        </div>
+        <div>
+        <h1 className='text-center'>Coming Soon</h1>
+        </div>
+         
+      </main>
+  )
+}
